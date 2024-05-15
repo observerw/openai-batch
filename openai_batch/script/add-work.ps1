83fae5cc1a12ddb44c435310eb501f9394030973
observer <wozluohd@gmail.com>
2024-05-15 15:58:18 +0800

    WIP: feat: using powershell script to add scheduled task

---
+++ openai_batch/script/add-work.ps1
@@ -0,0 +1,21 @@
+# $interpreter: path to the python interpreter
+# $script: python script content
+# $workDir: path to the work directory
+# $taskName: name of the task
+# $description: description of the task
+# $interval: time interval for the task to run (in seconds)
+# $completionWindow: time window for the task to complete (in seconds)
+
+$action = New-ScheduledTaskAction -Execute $interpreter -Argument "-c $script" -WorkingDirectory $workDir
+$settings = New-ScheduledTaskSettingsSet -DeleteExpiredTaskAfter (New-TimeSpan -Seconds 2)
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds($interval)
+$trigger.EndBoundary = (Get-Date).AddSeconds($completionWindow).ToString("s")
+
+Register-ScheduledTask \
+    -TaskName $taskName \
+    -Description $description \
+    -Action $action \
+    -Settings $settings \
+    -Trigger $trigger | Out-Null
+
+# TODO
